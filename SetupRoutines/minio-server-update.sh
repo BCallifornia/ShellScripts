2a0d92eb1b8b40c0cf2a3e604723fc6d92efec99
Brandon Callifornia <BCallifornia@users.noreply.github.com>
2020-02-09 19:46:11 +0100

    U minio-server use systemctl

--- SetupRoutines/minio-server-update.sh
+++ SetupRoutines/minio-server-update.sh
@@ -3,7 +3,7 @@
 # Description:
 #   Updates Minio Server
 #
-# Version: 0.0.5
+# Version: 0.1.6
 #
 # Author:
 #   Brandon Callifornia https://github.com/BCallifornia
@@ -11,9 +11,9 @@
 # --------------------------------------------------------------------------------------------
 OLDFILE=/usr/local/bin/minio
 wget -O minio.new https://dl.minio.io/server/minio/release/linux-amd64/minio
-sudo service minio stop
+sudo systemctl stop minio-server
 sudo chown minio:minio minio.new
 sudo chmod +x minio.new
 sudo mv /usr/local/bin/minio ./minio_"$(date -r $OLDFILE +"%Y%m%d%H%M")"
 sudo mv minio.new /usr/local/bin/minio
-sudo service minio start
+sudo systemctl start minio-server
